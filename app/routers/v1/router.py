a247711e16cc9b0f61814d3de2c277c4e5ea2c47
aashu10sh <pudasainiashutosh@gmail.com>
2025-07-27 20:23:20 +0545

    typo fix

--- app/routers/v1/router.py
+++ app/routers/v1/router.py
@@ -5,14 +5,14 @@ from app.core.dependencies.database import get_db
 from app.modules.auth.presentation.v1.router.auth_router import router as auth_router
 from app.modules.books.presentation.v1.router.books_router import router as books_router
 from app.modules.users.presentation.v1.router.users_router import router as users_router
-from app.modules.files.presentation.v1.router.files_router import router as file_router
+from app.modules.files.presentation.v1.router.files_router import router as files_router
 
 v1_router = APIRouter(prefix="/v1")
 
 v1_router.include_router(auth_router)
 v1_router.include_router(books_router)
 v1_router.include_router(users_router)
-v1_router.include_router(file_router)
+v1_router.include_router(files_router)
 
 
 @v1_router.get("/")
