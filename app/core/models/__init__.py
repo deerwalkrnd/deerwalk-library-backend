13ac53259349ad89d3eabd9140245cd457d3671d
Bijay Shrestha <bijayashrestha929@gmail.com>
2025-08-29 16:31:22 +0545

    book model

--- app/core/models/__init__.py
+++ app/core/models/__init__.py
@@ -7,6 +7,8 @@ from .users import UserModel as UserModel
 from .teacher_recommendation import (
     TeacherRecommendationModel as TeacherRecommendationModel,
 )
+from .book import BookModel as BookModel
+from .dss_book import DssBookModel as DssBookModel
 
 _all_ = [
     "UserModel",
@@ -17,4 +19,5 @@ _all_ = [
     "GenreModel",
     "EventModel",
     "TeacherRecommendationModel",
+    "BookModelDssBookModel",
 ]
