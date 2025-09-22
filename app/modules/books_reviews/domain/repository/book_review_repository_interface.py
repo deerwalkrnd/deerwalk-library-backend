079b7020b33e48855273f2365fec9471d582c49b
Bijay Shrestha <bijayashrestha929@gmail.com>
2025-09-22 15:31:17 +0545

    Add abstract method to count book reviews in repository interface

--- app/modules/books_reviews/domain/repository/book_review_repository_interface.py
+++ app/modules/books_reviews/domain/repository/book_review_repository_interface.py
@@ -1,6 +1,10 @@
+from abc import abstractmethod
 from app.core.domain.repositories.repository_interface import RepositoryInterface
 from app.modules.books_reviews.domain.entities.book_review import BookReview
 
 
 class BookReviewRepositoryInterface(RepositoryInterface[BookReview]):
-    pass
+    
+    @abstractmethod
+    async def count_book_reviews(self, book_id: int) -> int:
+        raise NotImplementedError
