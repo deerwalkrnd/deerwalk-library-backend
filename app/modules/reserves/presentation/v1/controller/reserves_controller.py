304d628bff88743348da513f1165e836de8982c9
aashu10sh <pudasainiashutosh@gmail.com>
2025-10-23 22:18:55 +0545

    reserves

--- app/modules/reserves/presentation/v1/controller/reserves_controller.py
+++ app/modules/reserves/presentation/v1/controller/reserves_controller.py
@@ -40,7 +40,7 @@ class ReservesController:
     async def reserve_book(
         self,
         reserve_book_request: ReserveBookRequest,
-        db: AsyncSession,
+        db: AsyncSession = Depends(get_db),
         user: User = Depends(get_current_user),
     ) -> Reserve | None:
         reserves_repository = ReservesRepository(db=db)
